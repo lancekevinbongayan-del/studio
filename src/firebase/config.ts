0f064b083269580ef8af2b4f047d422c71b8802d
Lance Kevin Bongayan <lancekevin.bongayan@neu.edu.ph>
2026-02-25 13:41:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8706353121-4d298",
+  "appId": "1:618058794261:web:5c57248f36c471d878d51c",
+  "apiKey": "AIzaSyBG8IbICOF8xBrd6wIgZgKedLdH1FC8C2A",
+  "authDomain": "studio-8706353121-4d298.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "618058794261"
+};
